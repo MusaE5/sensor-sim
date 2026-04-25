d130605911f338ad181361efd1a6535e768195ac
Musa Elashaal <elashaalmusa@gmail.com>
2026-04-25 14:14:49 -0400

    Modify IMUSensor.h to create random values for IMU data and return struct

--- include/IMUSensor.h
+++ include/IMUSensor.h
@@ -1,12 +1,29 @@
 #ifndef IMU_H
 #define IMU_H
 #include "Sensor.h"
+#include "SensorReading.h"
+#include <random>
+#include <string>
 using namespace std;
 
 class IMUSensor: public Sensor{
+    const string label = "IMU";
+    random_device rd;
+    mt19937 gen;
+    uniform_real_distribution<float> dist;
+
   public:
-    float read() override{
-        return 2.93;
+    IMUSensor(): gen(rd()), dist(-10.0, 10.0){};
+
+    SensorReading read() override{
+        SensorReading result;
+        result.label = label;
+
+        for(int i = 0; i<6; i++){
+            result.values.push_back(dist(gen));
+        }
+        
+        return result;
     }
 };
 
