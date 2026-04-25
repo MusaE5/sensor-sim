b3f25340b9c85d3abf22677248e16b69b37705af
Musa Elashaal <elashaalmusa@gmail.com>
2026-04-25 14:50:13 -0400

    Add random values to Ultrasonic and return struct

--- include/UltrasonicSensor.h
+++ include/UltrasonicSensor.h
@@ -1,12 +1,25 @@
 #ifndef ULTRASONIC_SENSOR_H
 #define ULTRASONIC_SENSOR_H
 #include "Sensor.h"
+#include "SensorReading.h"
+#include<random>
+#include<string>
 using namespace std;
 
 class UltrasonicSensor: public Sensor{
+    const string label = "Ultrasonic";
+    random_device rd;
+    mt19937 gen;
+    uniform_real_distribution<float> dist;
+
   public:
-    float read() override{
-        return 3.1;
+    UltrasonicSensor(): gen(rd()), dist(0, 100){}
+
+    SensorReading read() override{
+        SensorReading result;
+        result.label = label;
+        result.value = dist(gen);
+        return result;
     }
 };
 
