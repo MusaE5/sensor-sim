d2d1d66f693a64967779b552dec803dd0c1ea68c
Musa Elashaal <elashaalmusa@gmail.com>
2026-04-25 14:13:38 -0400

    Update Sensor.h to return Struct for read()

--- include/Sensor.h
+++ include/Sensor.h
@@ -1,10 +1,11 @@
 #ifndef SENSOR_H
 #define SENSOR_H
+#include "SensorReading.h"
 using namespace std;
 
 class Sensor{
     public:
-    virtual float read() = 0;
+    virtual SensorReading read() = 0;
 
     virtual ~Sensor() = default;
 };
